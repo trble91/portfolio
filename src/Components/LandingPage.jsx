118ca436245ee1d4950424e69b23ffed9a2cfb7a
trble91 <daleyaaron@gmail.com>
2023-05-23 22:24:30 -0400

    contemplating changing starting idea

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1>Aaron Daley</h1>
+      <img src="your-photo.jpg" alt="Profile" />
+      <p>As a Navy veteran, I developed a sharp eye for identifying gaps in the market. 
+        I'm driven to fill them through skillful approach, collaboration, plus continuous learning. 
+        Along with building software solutions, my passions for curating music and photography help me to seek innovation and aesthetic in digital product development.</p>
+      <div className="social-media-links">
+        <a href="https://www.linkedin.com/in/daleyaaron/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://github.com/trble91" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
