a3e6b63df1e61262473e64719497e8382ea5dbde
trble91 <daleyaaron@gmail.com>
2023-05-25 10:50:44 -0400

    Updates to theme switcher scraps rest of code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
+import Footer from "./Components/Footer.js";
+import NavBar from "./Components/Navbar.js";
 import "@/styles/globals.css";
-import { AnimatePresence } from "framer-motion";
-import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-mont",
-});
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -20,12 +14,10 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
-        className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
+        className={`${.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
       >
         <NavBar />
-        <AnimatePresence initial={false} mode="wait">
           <Component key={router.asPath} {...pageProps} />
-        </AnimatePresence>
         <Footer />
       </main>
     </>
